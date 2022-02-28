006f56ea9e46528dbad658e871623d204ca4950d
objects <mohamed.ramadan@objects.ws>
2022-02-28 23:11:43 +0200

    add migration and model of Discount

---
+++ database/migrations/2022_02_28_204948_create_discounts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInteger('type');
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->unsignedDouble('amount');
+            $table->unsignedTinyInteger('amount_type');
+            $table->unsignedInteger('generation_number');
+            $table->unsignedInteger('limit_usage');
+            $table->unsignedBigInteger('course_id')->index();
+            $table->unsignedBigInteger('speciality_id')->index();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
